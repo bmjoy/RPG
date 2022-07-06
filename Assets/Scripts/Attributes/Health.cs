fd3ef26e53cc56f98a10aa79d6188777d8c4199e
James LaFritz <ktmarine1999@gmail.com>
2022-07-06 12:24:33 -0400

    Added Die to Health

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -9,24 +9,96 @@ namespace RPG.Attributes
 {
     public class Health : MonoBehaviour
     {
+        #region Inspector Fields
+
+        /// <value>The maximum health of the object.</value>
         [SerializeField] float max = 100f;
+
+        #endregion
+
+        #region Private Fields
+
         private float m_value;
 
+        #endregion
+
+        #region Component References
+
+        #region Optional
+
+        /// <value>Cache the <a href="https://docs.unity3d.com/ScriptReference/Animator.html">UnityEngine.Animator</a></value>
+        private Animator m_animator;
+
+        private bool m_hasAnimator;
+        private static int _dieHash;
+
+        #endregion
+
+        #endregion
+
+        #region Properties
+
+        /// <value>Is the GameObject dead. (m_value == 0)</value>
+        public bool IsDead { get; private set; }
+
+        #endregion
+
+        #region Unity Messages
+
+        /// <summary>
+        /// <seealso href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html"/>
+        /// </summary>
         private void Awake()
         {
             m_value = max;
+
+            m_animator = GetComponentInChildren<Animator>();
+            m_hasAnimator = m_animator != null;
+
+            if (m_hasAnimator)
+            {
+                _dieHash = Animator.StringToHash("Die");
+            }
         }
 
+        #endregion
 
+        #region Public Methods
+
+        /// <summary>
+        /// If the Game Object is already Dead do nothing.
+        /// Reduce the value of the heath by the amount of damage.
+        /// If the health is less than 0, set the health to 0 and set the Die trigger if there is an animator.
+        /// </summary>
+        /// <param name="damage">The amount to reduce the health by.</param>
         public void TakeDamage(float damage)
         {
-            m_value = math.max(m_value - damage, 0);
-            if (m_value < 0)
-            {
-                m_value = 0;
-            }
+            if (IsDead) return;
+
+            m_value = math.min(math.max(m_value - damage, 0), max);
 
             Debug.Log($"{name} takes {damage} damage. Health is now {m_value}");
+
+            if (m_value == 0)
+            {
+                Die();
+            }
         }
+
+        #endregion
+
+        #region Private Methods
+
+        private void Die()
+        {
+            if (IsDead) return;
+            IsDead = true;
+            if (m_hasAnimator)
+            {
+                m_animator.SetTrigger(_dieHash);
+            }
+        }
+
+        #endregion
     }
 }
