851473c6b094e7d9734746e29d6c9a099478f163
James LaFritz <ktmarine1999@gmail.com>
2022-07-12 16:11:08 -0400

    Portal loads a new scene and set the player to the spawn point of the portal at the destination.

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -4,13 +4,14 @@
 
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     /// <summary>
     /// A <a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.html">UnityEngine.MonoBehavior</a> that
-    /// Telaports a player to another scene.
+    /// Teleports a player to another scene. You do not have to set the spawn point if the portal contains a Game Object with the name "SpawnPoint".
     /// <p>
     /// <a href="https://docs.unity3d.com/ScriptReference/RequireComponent.html">UnityEngine.RequireComponent</a>(
     /// typeof(<a href="https://docs.unity3d.com/2021.3/Documentation/ScriptReference/Playables.PlayableDirector.html">UnityEngine.Playables.PlayableDirector</a>)
@@ -19,7 +20,10 @@ namespace RPG.SceneManagement
     /// </summary>
     public class Portal : MonoBehaviour
     {
+        [SerializeField] private int portalIndex = 0;
         [SerializeField] private int sceneBuildIndex = -1;
+        [SerializeField] private int spawnPortalIndex = -1;
+        [SerializeField] private Transform spawnPoint;
 
         #region Component References
 
@@ -30,6 +34,19 @@ namespace RPG.SceneManagement
         #endregion
 
         #region Unity Messages
+
+        /// <summary>
+        /// <seealso href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html"/>
+        /// </summary>
+        private void Awake()
+        {
+            if (spawnPoint != null) return;
+            spawnPoint = transform.Find("SpawnPoint");
+
+            if (spawnPoint != null) return;
+            Debug.LogError("Portals require a spawn point to spawn the player at.");
+            gameObject.SetActive(false);
+        }
 
         /// <summary>
         /// <seealso href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.OnTriggerEnter.html"/>
@@ -46,10 +63,58 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneBuildIndex < 0)
+            {
+                Debug.LogWarning("Scene To Load is not Set");
+                yield break;
+            }
+
+            if (SceneManager.sceneCountInBuildSettings < sceneBuildIndex)
+            {
+                Debug.LogWarning($"Build Settings does not contain a scene at {sceneBuildIndex}");
+                yield break;
+            }
+
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneBuildIndex);
-            Debug.Log("Scene Loaded!");
+            yield return null;
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
             Destroy(gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            if (spawnPortalIndex == -1)
+            {
+                Debug.LogWarning("No Portal set.");
+                return null;
+            }
+
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.portalIndex != spawnPortalIndex) continue; 
+                return portal;
+            }
+
+            Debug.LogWarning($"Could not find a Portal with an index of {spawnPortalIndex} in scene {SceneManager.GetActiveScene().name}");
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            if (otherPortal == null) return;
+
+            GameObject player = GameObject.FindWithTag("Player");
+            if (player == null) return;
+            NavMeshAgent playerNaveMesh = player.GetComponent<NavMeshAgent>();
+            bool hasNavMesh = playerNaveMesh != null && playerNaveMesh.isActiveAndEnabled;
+
+            if (hasNavMesh) playerNaveMesh.enabled = false;
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+            if (hasNavMesh) playerNaveMesh.enabled = true;
         }
     }
 }
