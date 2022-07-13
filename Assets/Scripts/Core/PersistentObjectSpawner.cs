a31a452bf9fba3a2d9935af5f331738a2385b835
James LaFritz <ktmarine1999@gmail.com>
2022-07-13 01:09:32 -0400

    Added a way to persist objects between scenes

---
+++ Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,40 @@
+// PeristentObjectSpawner.cs
+// 07-13-2022
+// James LaFritz
+
+using UnityEngine;
+
+namespace RPG.Core
+{
+    /// <summary>
+    /// A <a href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.html">UnityEngine.MonoBehavior</a> that
+    /// spawns a <a href="https://docs.unity3d.com/ScriptReference/GameObject.html">GameObject</a> Prefab that persists between scenes.
+    /// <seealso href="https://docs.unity3d.com/ScriptReference/MonoBehaviour.html"/>
+    /// </summary>
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject persistentObjectPrefab;
+
+        private static bool _hasSpawned;
+
+        private void Awake()
+        {
+            if (persistentObjectPrefab == null)
+            {
+                Debug.LogError("PersistentObjectSpawner: No persistent object prefab assigned.");
+                return;
+            }
+
+            if (_hasSpawned) return;
+
+            _hasSpawned = true;
+            SpawnPersistentObjects();
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }
+}
