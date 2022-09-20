9da520d3e1bcd43384fd1ecb8f2725a82dceabb9
James LaFritz <ktmarine1999+git@gmail.com>
2022-09-20 18:40:07 -0400

    Added ability for weapon pickups to respawn.

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,17 +1,43 @@
+using System.Collections;
 using UnityEngine;
 
 namespace RPGEngine.Combat
 {
+    [RequireComponent(typeof(Collider))]
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
-        
+        [SerializeField] private float respawnTime = 5;
+
+        private Collider _collider;
+
+        private void Awake()
+        {
+            _collider = GetComponent<Collider>();
+        }
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
             Fighter fighter = other.GetComponent<Fighter>();
             if (fighter == null) return;
             fighter.EquipWeapon(weapon);
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowHidePickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowHidePickup(true);
+        }
+
+        private void ShowHidePickup(bool shouldShow)
+        {
+            _collider.enabled = shouldShow;
+            
+            foreach (Transform child in transform)
+                child.gameObject.SetActive(shouldShow);
         }
     }
 }
