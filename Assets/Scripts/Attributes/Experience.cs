525c90a4e5f13f3e669799b2591800b6884f60a7
James LaFritz <ktmarine1999+git@gmail.com>
2022-09-21 19:24:42 -0400

    Saved Experience Points

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,11 +1,30 @@
+using Newtonsoft.Json.Linq;
+using RPGEngine.Saving;
 using UnityEngine;
 
 namespace RPGEngine.Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISavable
     {
         [SerializeField] private float value;
 
         public void GainExperience(float amount) => value += amount;
+
+        #region Implementation of IJsonSavable
+
+        /// <inheritdoc />
+        public JToken CaptureAsJToken()
+        {
+            return JToken.FromObject(value);
+        }
+
+        /// <inheritdoc />
+        public void RestoreFromJToken(JToken state, int version)
+        {
+            if (state == null) return;
+            value = state.ToObject<float>();
+        }
+
+        #endregion
     }
 }
